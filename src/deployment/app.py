53795078d2f22d6ac3d3bc790759166b24414ce0
Sebastian Galindo Zaragoza <133563476+Sebastian-Zaragoza@users.noreply.github.com>
2025-09-03 23:54:16 -0600

    feat: Deployment file added

--- src/deployment/app.py
+++ src/deployment/app.py
@@ -10,7 +10,7 @@ sys.path.append(str(project_root))
 from src.utils.config import load_config
 
 config = load_config()
-model_path = project_root / config["paths"]["model_dir"] / "random_forest_pipeline.pkl"
+model_path = project_root / config["paths"]["model_dir"] / "random_forest_anxiety_level_pipeline.pkl"
 try:
     model_pipeline = joblib.load(model_path)
 except FileNotFoundError:
@@ -18,14 +18,13 @@ except FileNotFoundError:
 
 app = FastAPI(
     title="PredBit - API Habit Prediction",
-    description="API to predict who can keep a habit based on multiple features (actually it predicts anxiety levels, that is part of the four models selected to enhance this predictor)."
+    description="API to predict who can keep a habit based on multiple features (actually it predicts anxiety levels, that is part of the four features selected to enhance this predictor)."
 )
 
 class PredictionPayload(BaseModel):
     daily_screen_time_min: float
     notification_count: int
     social_media_time_min: float
-    mood_score: float
 
 @app.get("/anxiety-levels")
 def read_root():
@@ -38,7 +37,6 @@ def predict_habit(payload: PredictionPayload):
 
     result = "Your anxiety levels are low" if prediction == 1 else "Your anxiety levels are high"
     return {"prediction": result}
-
 
 if __name__ == "__main__":
     import uvicorn
